82f879ab3c5a68a87095ef45c13e0b1624d2f8ff
codeeeeeeeeeeee2201 <codeeeeeeeeeeee2201@gmail.com>
2023-02-26 18:57:49 +0900

    clone code

--- myhome/src/main/java/com/godcoder/myhome/MyhomeApplication.java
+++ myhome/src/main/java/com/godcoder/myhome/MyhomeApplication.java
@@ -1,11 +1,13 @@
 package com.godcoder.myhome;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+/*@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})*/
 public class MyhomeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyhomeApplication.class, args);
 	}
--- myhome/src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ myhome/src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -1,15 +1,28 @@
 package com.godcoder.myhome.controller;
 
+import com.godcoder.myhome.model.Board;
+import com.godcoder.myhome.repository.BoardRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    private BoardRepository boardRepository;
+
+    public BoardController(BoardRepository boardRepository){
+        this.boardRepository = boardRepository;
+    }
+
     @GetMapping("/list")
-    public String list(){
+    public String list(Model model){
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards",boards);
         return "board/list";
     }
 }
